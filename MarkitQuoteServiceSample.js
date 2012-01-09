901c89ae086b0eb1c9767600dddaadd8ff812f48
Mark Healey <mark.healey@markit.com>
2012-01-09 13:45:10 -0700

    Initial commit

---
+++ MarkitQuoteServiceSample.js
@@ -0,0 +1,57 @@
+﻿/** 
+ * Version 1.0, Jan 2012
+ */
+
+var Markit = {};
+/**
+* Define the QuoteService.
+* First argument is symbol (string) for the quote. Examples: AAPL, MSFT, JNJ, GOOG.
+* Second argument is fCallback, a callback function executed onSuccess of API.
+*/
+Markit.QuoteService = function(sSymbol, fCallback) {
+    this.symbol = sSymbol;
+    this.fCallback = fCallback;
+    this.DATA_SRC = "http://jobs.markitondemand.com/Api/Quote/jsonp";
+    this.makeRequest();
+};
+/**
+* Ajax success callback. fCallback is the 2nd argument in the QuoteService constructor.
+*/
+Markit.QuoteService.prototype.handleSuccess = function(jsonResult) {
+    this.fCallback(jsonResult);
+};
+/**
+* Ajax error callback
+*/
+Markit.QuoteService.prototype.handleError = function(jsonResult) {
+    console.error(jsonResult);
+};
+/** 
+* Starts a new ajax request to the Quote API
+*/
+Markit.QuoteService.prototype.makeRequest = function() {
+    //Abort any open requests
+    if (this.xhr) { this.xhr.abort(); }
+    //Start a new request
+    this.xhr = $.ajax({
+        data: { symbol: this.symbol },
+        url: this.DATA_SRC,
+        dataType: "jsonp",
+        success: this.handleSuccess,
+        error: this.handleError,
+        context: this
+    });
+};
+
+new Markit.QuoteService("AAPL", function(jsonResult) {
+    //If all goes well, your quote will be here.
+    console.log(jsonResult);
+
+    //Now proceed to do something with the data.
+    $("h1").first().text(jsonResult.Data.Name);
+
+    /**
+    * Need help? Visit the API documentation at:
+    * http://jobs.markitondemand.com/MarketDataApi
+    */
+});
---
+++ MarkitTimeseriesServiceSample.js
@@ -0,0 +1,134 @@
+﻿/** 
+ * Version 1.0, Jan 2012
+ */
+var Markit = {};
+/**
+ * Define the TimeseriesService.
+ * First argument is symbol (string) for the quote. Examples: AAPL, MSFT, JNJ, GOOG.
+ * Second argument is duration (int) for how many days of history to retrieve.
+ * Third argument is fCallback, a callback function executed onSuccess of API.
+ */
+Markit.TimeseriesService = function(symbol,duration,fCallback){
+    this.symbol = symbol;
+    this.duration = duration;
+    this.fCallback = fCallback;
+    this.PlotChart();
+};
+
+Markit.TimeseriesService.prototype.PlotChart = function(){
+    
+    //Make JSON request for timeseries data
+    $.ajax({
+        beforeSend:function(){
+            $("#chartDemoContainer").text("Loading chart...");
+        },
+        data: { 
+            symbol: this.symbol, 
+            duration: this.duration 
+        },
+        url: "http://jobs.markitondemand.com/Api/Timeseries/jsonp",
+        dataType: "jsonp",
+        context: this,
+        success: function(json){
+            this.BuildDataAndChart(json);
+        },
+        error: function(){
+            alert("Couldn't generate chart.");
+        }
+    });
+};
+
+Markit.TimeseriesService.prototype.BuildDataAndChart = function(json){
+    var dateDS = json.Data.SeriesDates,
+        closeDS = json.Data.Series.close.values,
+        openDS = json.Data.Series.open.values,
+        closeDSLen = closeDS.length,
+        irregularIntervalDS = [];
+	
+    //Build array of arrays of date & price values
+    //Market data is inherently irregular 
+    //and HighCharts likes regularity (for axis intervals)
+    for (var i=0; i<closeDSLen;i++){
+        var dat = new Date(dateDS[i]);
+        var dateIn = Date.UTC(dat.getFullYear(), dat.getMonth(), dat.getDate());
+        var val = closeDS[i];
+        irregularIntervalDS.push([dateIn,val]);	
+    }
+	
+    //set dataset and chart label
+    this.oChartOptions.series[0].data = irregularIntervalDS;
+    this.oChartOptions.title.text = "Price History of " + json.Data.Name + " (1 year)";
+    
+    //init chart
+    new Highcharts.Chart(this.oChartOptions);
+};
+
+//Define the HighCharts options
+Markit.TimeseriesService.prototype.oChartOptions = {
+	chart: {
+		renderTo: 'chartDemoContainer'
+	},
+	title:{},
+	subtitle: {
+		text: 'Source: Thomson Reuters DataScope / Markit On Demand'
+	},
+	xAxis: {
+		type: 'datetime'
+	},
+	yAxis: [{ // left y axis
+		title: {
+			text: null
+		},
+		labels: {
+			align: 'left',
+			x: 3,
+			y: 16,
+			formatter: function() {
+				return Highcharts.numberFormat(this.value, 0);
+			}
+		},
+		showFirstLabel: false
+	}, { // right y axis
+		linkedTo: 0,
+		gridLineWidth: 0,
+		opposite: true,
+		title: {
+			text: null
+		},
+		labels: {
+			align: 'right',
+			x: -3,
+			y: 16,
+			formatter: function() {
+				return Highcharts.numberFormat(this.value, 0);
+			}
+		},
+		showFirstLabel: false
+	}],
+	tooltip: {
+		shared: true,
+		crosshairs: true
+	},
+	plotOptions: {
+		series: {
+			marker: {
+				lineWidth: 1
+			}
+		}
+	},
+	series: [{
+		name: "Close price",
+		lineWidth: 2,
+		marker: {
+			radius: 0
+		}
+	}]
+	//,credits:{ enabled:false },
+};
+
+new Markit.TimeseriesService("GOOG", 365);
+
+/**
+* Need help? Visit the API documentation at:
+* http://jobs.markitondemand.com/MarketDataApi
+*/
