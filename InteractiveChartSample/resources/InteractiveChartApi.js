ccab2028887b7785a57753abc01ab2dfc1064a95
Mark Healey <mark.healey@markit.com>
2013-10-25 15:35:22 -0600

    interactive chart demo

---
+++ InteractiveChartSample/resources/InteractiveChartApi.js
@@ -0,0 +1,315 @@
+/** 
+ * Version 2.0
+ */
+var Markit = {};
+/**
+ * Define the InteractiveChartApi.
+ * First argument is symbol (string) for the quote. Examples: AAPL, MSFT, JNJ, GOOG.
+ * Second argument is duration (int) for how many days of history to retrieve.
+ */
+Markit.InteractiveChartApi = function(symbol,duration){
+    this.symbol = symbol.toUpperCase();
+    this.duration = duration;
+    this.PlotChart();
+};
+
+Markit.InteractiveChartApi.prototype.PlotChart = function(){
+    
+    var params = {
+        parameters: JSON.stringify( this.getInputParams() )
+    }
+
+    //Make JSON request for timeseries data
+    $.ajax({
+        beforeSend:function(){
+            $("#chartDemoContainer").text("Loading chart...");
+        },
+        data: params,
+        url: "http://dev.markitqa.com/Api/v2/InteractiveChart/jsonp",
+        dataType: "jsonp",
+        context: this,
+        success: function(json){
+            //Catch errors
+            if (!json || json.Message){
+                console.error("Error: ", json.Message);
+                return;
+            }
+            this.render(json);
+        },
+        error: function(response,txtStatus){
+            console.log(response,txtStatus)
+        }
+    });
+};
+
+Markit.InteractiveChartApi.prototype.getInputParams = function(){
+    return {  
+        Normalized: false,
+        NumberOfDays: this.duration,
+        DataPeriod: "Day",
+        Elements: [
+            {
+                Symbol: this.symbol,
+                Type: "price",
+                Params: ["ohlc"] //ohlc, c = close only
+            },
+            {
+                Symbol: this.symbol,
+                Type: "volume"
+            }
+        ]
+        //,LabelPeriod: 'Week',
+        //LabelInterval: 1
+    }
+};
+
+Markit.InteractiveChartApi.prototype._fixDate = function(dateIn) {
+    var dat = new Date(dateIn);
+    return Date.UTC(dat.getFullYear(), dat.getMonth(), dat.getDate());
+};
+
+Markit.InteractiveChartApi.prototype.BuildDataAndChart = function(json){
+    console.log(json)
+
+    var dates = json.Dates || [];
+    var elements = json.Elements || [];
+    var chartSeries = [];
+
+    if (elements[0]){
+
+        for (var i = 0, datLen = dates.length; i < datLen; i++) {
+            var dat = this._fixDate( dates[i] );
+            var pointData = [
+                dat,
+                elements[0].DataSeries['open'].values[i],
+                elements[0].DataSeries['high'].values[i],
+                elements[0].DataSeries['low'].values[i],
+                elements[0].DataSeries['close'].values[i]
+            ];
+            chartSeries.push( pointData );
+        };
+    }
+
+    var options = this.oChartOptions;
+
+    options.series[0] =
+        {
+            name: this.symbol,
+            data: chartSeries,
+            color: '#fff',
+            tooltip: {
+                valueDecimals: 2
+            },
+            type: 'ohlc'
+        };
+
+
+    $('#chartDemoContainer').highcharts('StockChart', options);
+
+};
+
+Markit.InteractiveChartApi.prototype._getOHLC = function(json) {
+    var dates = json.Dates || [];
+    var elements = json.Elements || [];
+    var chartSeries = [];
+
+    if (elements[0]){
+
+        for (var i = 0, datLen = dates.length; i < datLen; i++) {
+            var dat = this._fixDate( dates[i] );
+            var pointData = [
+                dat,
+                elements[0].DataSeries['open'].values[i],
+                elements[0].DataSeries['high'].values[i],
+                elements[0].DataSeries['low'].values[i],
+                elements[0].DataSeries['close'].values[i]
+            ];
+            chartSeries.push( pointData );
+        };
+    }
+    return chartSeries;
+};
+
+Markit.InteractiveChartApi.prototype._getVolume = function(json) {
+    var dates = json.Dates || [];
+    var elements = json.Elements || [];
+    var chartSeries = [];
+
+    if (elements[1]){
+
+        for (var i = 0, datLen = dates.length; i < datLen; i++) {
+            var dat = this._fixDate( dates[i] );
+            var pointData = [
+                dat,
+                elements[1].DataSeries['volume'].values[i]
+            ];
+            chartSeries.push( pointData );
+        };
+    }
+    return chartSeries;
+};
+
+Markit.InteractiveChartApi.prototype.render = function(data) {
+    //console.log(data)
+    // split the data set into ohlc and volume
+    var ohlc = this._getOHLC(data),
+        volume = this._getVolume(data);
+
+    // set the allowed units for data grouping
+    var groupingUnits = [[
+        'week',                         // unit name
+        [1]                             // allowed multiples
+    ], [
+        'month',
+        [1, 2, 3, 4, 6]
+    ]];
+
+    // create the chart
+    $('#chartDemoContainer').highcharts('StockChart', {
+        
+        rangeSelector: {
+            selected: 1
+            //enabled: false
+        },
+
+        title: {
+            text: this.symbol + ' Historical Price'
+        },
+
+        yAxis: [{
+            title: {
+                text: 'OHLC'
+            },
+            height: 200,
+            lineWidth: 2
+        }, {
+            title: {
+                text: 'Volume'
+            },
+            top: 300,
+            height: 100,
+            offset: 0,
+            lineWidth: 2
+        }],
+        
+        series: [{
+            type: 'candlestick',
+            name: this.symbol,
+            data: ohlc,
+            dataGrouping: {
+                units: groupingUnits
+            }
+        }, {
+            type: 'column',
+            name: 'Volume',
+            data: volume,
+            yAxis: 1,
+            dataGrouping: {
+                units: groupingUnits
+            }
+        }],
+        credits: {
+            enabled:false
+        }
+    });
+};
+
+//Define the HighCharts options
+var fontface = '"Helvetica Neue",Helvetica,Arial,sans-serif';
+
+Markit.InteractiveChartApi.prototype.oChartOptions = {
+    rangeSelector: {
+        selected: 1
+    },
+
+    title: { },
+
+    yAxis: [{
+        title: {
+            text: 'OHLC'
+        },
+        height: 200,
+        lineWidth: 2
+    }, {
+        title: {
+            text: 'Volume'
+        },
+        top: 300,
+        height: 100,
+        offset: 0,
+        lineWidth: 2
+    }],
+    
+    series: []
+}
+
+Markit.InteractiveChartApi.prototype.oChartOptions___ = {
+    chart: {
+        backgroundColor: '#0B629A'
+    },
+    rangeSelector : {
+        enabled: false
+    },
+    navigator: {
+        maskFill: 'rgba(255, 255, 255, 0.75)'
+    },
+    title : { 
+        style: {
+            color: '#fff',
+            font: 'normal 16px ' + fontface
+        }
+    },
+    subtitle: {
+      style: {
+         color: '#fff',
+         font: 'normal 12px ' + fontface
+      }
+   },
+   xAxis: {
+      gridLineWidth: 1,
+      lineColor: '#86c9f4',
+      tickColor: '#86c9f4',
+      labels: {
+         style: {
+            color: '#fff',
+            font: '11px '  + fontface
+         }
+      },
+      title: {
+         style: {
+            color: '#fff',
+            fontWeight: 'bold',
+            fontSize: '12px',
+            fontFamily: fontface
+
+         }
+      }
+   },
+   yAxis: {
+      labels: {
+         style: {
+            color: '#fff',
+            font: '11px' + fontface
+         }
+      },
+      title: {
+         style: {
+            color: '#fff',
+            fontWeight: 'bold',
+            fontSize: '12px',
+            fontFamily: fontface
+         }
+      }
+   },
+   labels: {
+      style: {
+         color: '#fff'
+      }
+   },
+    exporting: {
+        enabled: true
+    },
+    series : [],
+    credits:{ enabled:false }
+    
+};
